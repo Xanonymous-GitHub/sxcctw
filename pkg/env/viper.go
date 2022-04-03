3359d2e165c08a1f5c70c8576a6b9575c4e4e99a
Xanonymous <trusaidlin@gmail.com>
2022-04-03 23:36:56 +0800

    chore: add some envs

--- pkg/env/viper.go
+++ pkg/env/viper.go
@@ -6,13 +6,14 @@ var (
 	IsDebugMode             bool
 	DBGrpcServerPort        string
 	DBGrpcServerMetricsPort string
+	DBGrpcServerHost        string
 	DBUserName              string
 	DBPassword              string
 	DBHost                  string
 	DBPort                  string
 	DBName                  string
-
-	HttpServerPort string
+	ApiRestServerPort       string
+	ApiRootPath             string
 )
 
 func init() {
@@ -20,11 +21,12 @@ func init() {
 	IsDebugMode = viper.GetBool("DEBUG")
 	DBGrpcServerPort = viper.GetString("DB_GRPC_SEVER_PORT")
 	DBGrpcServerMetricsPort = viper.GetString("DB_GRPC_SERVER_METRICS_PORT")
+	DBGrpcServerHost = viper.GetString("DB_GRPC_SERVER_HOST")
 	DBUserName = viper.GetString("DB_USERNAME")
 	DBPassword = viper.GetString("DB_PASSWORD")
 	DBHost = viper.GetString("DB_HOST")
 	DBPort = viper.GetString("DB_PORT")
 	DBName = viper.GetString("DB_NAME")
-
-	HttpServerPort = viper.GetString("HTTP_SERVER_PORT")
+	ApiRestServerPort = viper.GetString("API_REST_SERVER_PORT")
+	ApiRootPath = viper.GetString("API_ROOT_PATH")
 }
