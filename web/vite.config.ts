752f990704eaa29d1a05c3f185e4d64dc630c871
Xanonymous <trusaidlin@gmail.com>
2022-04-21 06:55:53 +0800

    chore: add html plugin

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -7,6 +7,7 @@ import Pages from 'vite-plugin-pages'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Unocss from 'unocss/vite'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 export default defineConfig({
   resolve: {
@@ -15,7 +16,9 @@ export default defineConfig({
     },
   },
   plugins: [
-    Vue(),
+    Vue({
+      isProduction: true,
+    }),
 
     // https://github.com/hannoeru/vite-plugin-pages
     Pages(),
@@ -38,7 +41,30 @@ export default defineConfig({
     // https://github.com/antfu/unocss
     // see unocss.config.ts for config
     Unocss(),
+    createHtmlPlugin({
+      minify: true,
+    }),
   ],
+
+  build: {
+    minify: 'terser',
+    brotliSize: true,
+    cssCodeSplit: true,
+    terserOptions: {
+      sourceMap: false,
+      ecma: 5,
+      ie8: true,
+      safari10: true,
+      keep_classnames: false,
+      keep_fnames: false,
+      compress: true,
+      format: {
+        preserve_annotations: false,
+        comments: false,
+      },
+    },
+    chunkSizeWarningLimit: 100000,
+  },
 
   // https://github.com/vitest-dev/vitest
   test: {
